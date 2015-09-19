2ee41e40c3ad938f92eeef4e31dca24ba1121a25
alexro <alex.rodygin@mongrow.com>
2015-09-19 22:40:34 +0100

    Initial

---
+++ FFLinkMain/FrmMain.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace FFLinkMain
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrScreenshot = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrScreenshot
+            // 
+            this.tmrScreenshot.Enabled = true;
+            this.tmrScreenshot.Interval = 1000;
+            this.tmrScreenshot.Tick += new System.EventHandler(this.tmrScreenshot_Tick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrScreenshot;
+    }
+}
+
---
+++ FFLinkMain/FrmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FFLinkMain
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrScreenshot_Tick(object sender, EventArgs e)
+        {
+            tmrScreenshot.Enabled = false;
+
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(bmp as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+            bmp.Save(@"C:\Temp\shot.jpg", ImageFormat.Jpeg);
+            Text = "Done";
+        }
+    }
+}
